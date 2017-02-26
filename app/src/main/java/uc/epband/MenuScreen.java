a7a4c8e40dec87fc96b54fcce67b6d17c0ce5f90
AndrewAlbert <andrew.christian.albert@gmail.com>
2017-02-26 00:51:48 -0500

    Update with profile settings activity

--- app/src/main/java/uc/epband/MenuScreen.java
+++ app/src/main/java/uc/epband/MenuScreen.java
@@ -2,7 +2,9 @@ package uc.epband;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,9 +19,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -28,7 +36,7 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.lang.Math;
 public class MenuScreen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -38,15 +46,6 @@ public class MenuScreen extends AppCompatActivity
         setContentView(R.layout.activity_menu_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,9 +82,9 @@ public class MenuScreen extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
         return super.onOptionsItemSelected(item);
     }
 
@@ -106,38 +105,23 @@ public class MenuScreen extends AppCompatActivity
                 System.out.println("Menu: Workout Templates");
                 CreateFile("TestWorkout");
                 ReadFile("TestWorkout");
-                //R.id.
                 break;
             case R.id.nav_profile:
                 System.out.println("Menu: Profile");
-                int[] profile = GetProfile("test");
-                System.out.println("height: " + profile[0]);
-
-                int[] measure = GetHeight(profile[0]);
-                System.out.println(measure[0] + "'" + measure[1] + "\"");
-                System.out.println("weight: " + profile[1]);
-                System.out.println("age: " + profile[2]);
-                System.out.println("gender: " + profile[3]);
+                Intent intent = new Intent(MenuScreen.this, ProfileActivity.class);
+                startActivity(intent);
                 break;
             case R.id.nav_review:
                 System.out.println("Menu: View Analysis");
-                LineChart chart = (LineChart) findViewById(R.id.chart);
-
-                List<Entry> entries = new ArrayList<Entry>();
-                int[] X = {0,1,2,3,4,5,6,7,8,9};
-                int[] Y = {0,1,4,9,16,25,36,49,64,81};
-                for (int i = 0; i < 10; i++){
-                    entries.add(new Entry(X[i],Y[i]));
-                }
-                LineDataSet dataSet = new LineDataSet(entries, "Line Graph");
-                LineData lineData = new LineData(dataSet);
-                chart.setData(lineData);
-                chart.invalidate(); // refresh
+                //PieChart chart = (PieChart) findViewById(R.id.chart);
+                CreateHeartRateSummary((PieChart) findViewById(R.id.chart), 25.0f, 33.0f, 42.0f, 10.0f);
                 System.out.println("New Chart");
                 break;
             case R.id.nav_workout_start:
                 System.out.println("Menu: Start Workout");
-                ListFiles();
+                PrintProfile();
+                //CreateHeartRateSummary((PieChart) findViewById(R.id.chart), 5.0f, 15.0f, 40.0f, 40.0f);
+                //ListFiles();
                 break;
         }
 
@@ -146,39 +130,98 @@ public class MenuScreen extends AppCompatActivity
         return true;
     }
 
+    public void CreateHeartRateSummary(PieChart chart, float unread, float rest, float aerobic, float anaerobic){
+        // CREATE DATA
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(unread, "Unread"));
+        entries.add(new PieEntry(rest, "Resting"));
+        entries.add(new PieEntry(aerobic, "Aerobic"));
+        entries.add(new PieEntry(anaerobic, "Anaerobic"));
+        PieDataSet set = new PieDataSet(entries, "Heart Rate Over Workout");
+
+        // CHART STYLE SETTINGS
+        set.setColors(Color.rgb(160, 160, 160), Color.rgb(0, 128, 255), Color.rgb(255, 255, 0), Color.rgb(255, 0, 0));
+        PieData data = new PieData(set);
+        data.setValueTextSize(16.0f);
+        data.setValueTextColor(Color.BLACK);
+        chart.setEntryLabelTextSize(24.0f);
+        chart.setUsePercentValues(true);
+        chart.setHoleRadius(0f);
+        chart.setTransparentCircleAlpha(0);
+
+        if(true){
+            // Don't use labels on chart
+            chart.setDrawEntryLabels(false);
+        }
+        else{
+            // Use labels on chart
+            chart.setDrawEntryLabels(true);
+            chart.setEntryLabelColor(Color.rgb(0, 0, 0));
+        }
+
+        // LEGEND SETTINGS
+        Legend legend = chart.getLegend();
+        legend.setEnabled(true);
+        legend.setTextColor(Color.rgb(255, 255, 255));
+        legend.setTextSize(16.0f);
+        //legend.setTypeface(Typeface TF);
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        legend.setWordWrapEnabled(true);
+
+        // SET DATA AND UPDATE CHART
+        chart.setDescription(null);
+        chart.setData(data);
+        chart.invalidate(); // refresh
+    }
+
+    public void PrintProfile(){
+        int[] profile = GetProfile("test");
+        System.out.println("height: " + profile[0]);
+        int[] measure = GetHeight(profile[0]);
+        System.out.println(measure[0] + "'" + measure[1] + "\"");
+        System.out.println("weight: " + profile[1]);
+        System.out.println("age: " + profile[2]);
+        System.out.println("gender: " + profile[3]);
+    }
+
+    public void CreateLineChart(){
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+        List<Entry> entries = new ArrayList<Entry>();
+        float Y;
+        for (int X = 0; X < 360*100; X++){
+            Y = ((float) Math.cos(Math.toRadians(X)));
+            entries.add(new Entry(X,Y));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Line Graph");
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+    }
+
     public String NewFilename(String WorkoutName){
         //Returns the current date and time used for the file
         String filename = WorkoutName + DateFormat.getDateTimeInstance().format(new Date());
         String[] List = fileList();
         for(String f: List){
             if(f == filename){
-                filename = filename + "(1)";
-                break;
+                filename = filename + "-";
             }
         }
         return filename;
-    };
-
-    public void SetProfile(String filename, int height, int weight, int age, boolean gender){
-        // gender: 0 is male, 1 is female
-        SharedPreferences Settings = getSharedPreferences(filename,MODE_PRIVATE);
-        SharedPreferences.Editor editor = Settings.edit();
-        editor.putInt("height", height);
-        editor.putInt("weight", weight);
-        editor.putInt("age", age);
-        editor.putInt("gender", ((gender) ? 1 : 0) );
-        editor.apply();
-    };
+    }
 
     public int[] GetProfile(String filename){
         SharedPreferences Settings = getSharedPreferences(filename,MODE_PRIVATE);
-        int[] profile = new int[4];
+        int[] profile = new int[6];
         profile[0] = Settings.getInt("height", 70);
         profile[1] = Settings.getInt("weight", 170);
-        profile[2] = Settings.getInt("age", 21);
+        profile[2] = Settings.getInt("age", 20);
         profile[3] = Settings.getInt("gender", 0);
+        profile[4] = Settings.getInt("MHR", 200);
+        profile[5] = Settings.getInt("arm", 32);
         return profile;
-    };
+    }
 
     public void ListFiles(){
         File Dir = getFilesDir();
@@ -191,7 +234,7 @@ public class MenuScreen extends AppCompatActivity
         for(String f: List){
             System.out.println(f);
         }
-    };
+    }
 
     public void CreateFile(String filename){
         try{
@@ -203,7 +246,7 @@ public class MenuScreen extends AppCompatActivity
         } catch (Exception ex){
             System.out.println("Well fuck... Can't open files");
         }
-    };
+    }
 
     public void AppendFile(String filename, String message){
         try {
@@ -213,7 +256,7 @@ public class MenuScreen extends AppCompatActivity
         } catch (Exception ex){
             System.out.println("Well fuck... Can't append to files");
         }
-    };
+    }
 
     public void ReadFile(String filename){
         try{
@@ -226,18 +269,17 @@ public class MenuScreen extends AppCompatActivity
         } catch(Exception ex){
             System.out.println("Well fuck... Can't read from files");
         }
-    };
+    }
 
     public int[] GetHeight(int height){
         int[] measure = new int[2];
         measure[0] = height/12;
         measure[1] = height - 12*measure[0];
         return measure;
-    };
+    }
 
     public int GetInches(int feet, int inches){
         return (12*feet + inches);
     }
-
 
 }
---
+++ app/src/main/java/uc/epband/ProfileActivity.java
@@ -0,0 +1,100 @@
+package uc.epband;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int[] profile = GetProfile("test");
+        EditText Age = (EditText) findViewById(R.id.editTextAge);
+        EditText Height = (EditText) findViewById(R.id.editTextHeight);
+        EditText Weight = (EditText) findViewById(R.id.editTextWeight);
+        ToggleButton Gender = (ToggleButton) findViewById(R.id.toggleButtonGender);
+
+
+        Height.setText(Integer.toString(profile[0]));
+        Weight.setText(Integer.toString(profile[1]));
+        Age.setText(Integer.toString(profile[2]));
+
+        if(profile[3] == 1){
+            Gender.setChecked(true);
+        }
+        else{
+            Gender.setChecked(false);
+        }
+
+        FloatingActionButton commit = (FloatingActionButton) findViewById(R.id.commit);
+        commit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // COMMIT ALL CHANGES TO THE SHARED PREFERENCES PROFILE
+                EditText Age = (EditText) findViewById(R.id.editTextAge);
+                EditText Height = (EditText) findViewById(R.id.editTextHeight);
+                EditText Weight = (EditText) findViewById(R.id.editTextWeight);
+                ToggleButton Gender = (ToggleButton) findViewById(R.id.toggleButtonGender);
+
+                int age = Integer.parseInt(Age.getText().toString());
+                int height = Integer.parseInt(Height.getText().toString());
+                int weight = Integer.parseInt(Weight.getText().toString());
+                boolean gender = Gender.isActivated();
+
+                SetProfile("test", height, weight, age, gender);
+                finish();
+            }
+        });
+
+        FloatingActionButton cancel = (FloatingActionButton) findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // REJECT ALL CHANGES
+                finish();
+            }
+        });
+    }
+
+    public void SetProfile(String filename, int height, int weight, int age, boolean gender){
+        // gender: 0 is male, 1 is female
+        if(age < 10) age = 18;
+        else if (age > 100) age = 100;
+        int MHR = 220 - age;
+
+        if(height < 58) height = 58;
+        else if(height > 96) height = 70;
+        int ArmLength = (height*9/20); //assume each arm is 90% the height
+
+        SharedPreferences Settings = getSharedPreferences(filename,MODE_PRIVATE);
+        SharedPreferences.Editor editor = Settings.edit();
+        editor.putInt("height", height);
+        editor.putInt("weight", weight);
+        editor.putInt("age", age);
+        editor.putInt("gender", ((gender) ? 1 : 0) );
+        editor.putInt("MHR", MHR);
+        editor.putInt("arm", ArmLength);
+        editor.apply();
+    }
+
+    public int[] GetProfile(String filename){
+        SharedPreferences Settings = getSharedPreferences(filename,MODE_PRIVATE);
+        int[] profile = new int[4];
+        profile[0] = Settings.getInt("height", 70);
+        profile[1] = Settings.getInt("weight", 170);
+        profile[2] = Settings.getInt("age", 21);
+        profile[3] = Settings.getInt("gender", 0);
+        return profile;
+    }
+}
